1b067394bcc7ff659596611bae0cfc345b4d28d7
Bonae <bonae@localhost.localdomain>
2023-04-14 05:00:32 +0300

    api frontend connection

--- dashboard/src/features/authapi.js
+++ dashboard/src/features/authapi.js
@@ -1,11 +1,13 @@
 import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 export const api = createApi({
-    baseQuery:fetchBaseQuery(""),
+    baseQuery:fetchBaseQuery({baseUrl: "http://localhost:8000"}),
     reducerPath:"authapi",
     tagTypes:["uaser"],
     endpoints: (build)=>({
         getUsers: build.query({
-             query: ()=>`user/all`,
+             query: ()=>`/api/auth/all-users`,
+              
         })
     })
 })
+export const { useGetUsersQuery } = api;
